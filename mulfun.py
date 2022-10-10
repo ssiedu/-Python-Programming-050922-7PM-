6b3389f847064af7915b22c162845794efde788c
ankitaSsi <104835257+ankitaSsi@users.noreply.github.com>
2022-10-10 19:57:04 +0530

    program

---
+++ mulfun.py
@@ -0,0 +1,13 @@
+def mul(x,y,z):
+    res=x*y*z;
+    return res
+
+
+
+a=int(input("Enter the value of a : "))
+b= int(input("Enter the value of b : "))
+c= int(input("Enter the value of c : "))
+
+
+result=mul(a,b,c)
+print("mul is : ",result)
